e836be80dba89d29f639c9431d554296d3f06e1f
1600133971 <34409521+1600133971@users.noreply.github.com>
2018-04-16 23:43:39 +0800

    add bat

--- Makefile.bat
+++ Makefile.bat
@@ -1,9 +1,29 @@
-cat src/header.js src/defaults.js src/utils.js src/simpledraw.js src/rangemap.js src/interact.js src/base.js src/chart-line.js src/chart-bar.js src/chart-tristate.js src/chart-discrete.js src/chart-bullet.js src/chart-pie.js src/chart-box.js src/vcanvas-base.js src/vcanvas-canvas.js src/vcanvas-vml.js src/footer.js | sed "s/@VERSION@/2.13/" >dist/jquery.sparkline.js
+@echo off
+set SRC_DIR=src
+set DIST_DIR=dist
+set VERSION=2.1.3
+set SRC_FILES=^
+%SRC_DIR%/header.js ^
+%SRC_DIR%/defaults.js ^
+%SRC_DIR%/utils.js ^
+%SRC_DIR%/simpledraw.js ^
+%SRC_DIR%/rangemap.js ^
+%SRC_DIR%/interact.js ^
+%SRC_DIR%/base.js ^
+%SRC_DIR%/chart-line.js ^
+%SRC_DIR%/chart-bar.js ^
+%SRC_DIR%/chart-tristate.js ^
+%SRC_DIR%/chart-discrete.js ^
+%SRC_DIR%/chart-bullet.js ^
+%SRC_DIR%/chart-pie.js ^
+%SRC_DIR%/chart-box.js ^
+%SRC_DIR%/vcanvas-base.js ^
+%SRC_DIR%/vcanvas-canvas.js ^
+%SRC_DIR%/vcanvas-vml.js ^
+%SRC_DIR%/footer.js
 
-cat minheader.txt | sed "s/@VERSION@/2.13/" >dist/jquery.sparkline.min.js
-
-uglifyjs dist/jquery.sparkline.js >>dist/jquery.sparkline.min.js
-
-gzip -9 < dist/jquery.sparkline.js >dist/jquery.sparkline.js.gz
-
-gzip -9 < dist/jquery.sparkline.min.js >dist/jquery.sparkline.min.js.gz
+call cat %SRC_FILES% | sed "s/@VERSION@/%VERSION%/" >%DIST_DIR%/jquery.sparkline.js
+call cat minheader.txt | sed "s/@VERSION@/%VERSION%/" >%DIST_DIR%/jquery.sparkline.min.js
+call uglifyjs %DIST_DIR%/jquery.sparkline.js  >>%DIST_DIR%/jquery.sparkline.min.js
+call gzip -9 < %DIST_DIR%/jquery.sparkline.js >%DIST_DIR%/jquery.sparkline.js.gz
+call gzip -9 < %DIST_DIR%/jquery.sparkline.min.js >%DIST_DIR%/jquery.sparkline.min.js.gz
